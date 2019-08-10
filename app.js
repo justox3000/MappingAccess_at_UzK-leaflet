a324df868937c82ff5c0596c38c499233ddb4af7
Justus Lambrecht <justuslambrecht@icloud.com>
2019-08-10 02:32:52 +0200

    Adding Zoom Control

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // Initialize leaflet.js
 var L = require('leaflet');
 
+
 // Initialize the map
 var map = L.map('map', {
-  scrollWheelZoom: false
+  scrollWheelZoom: false,
+  zoomControl: false
 });
 
 // Set the position and zoom level of the map
@@ -14,3 +16,12 @@ var osm_mapnik = L.tileLayer('https://api.mapbox.com/styles/v1/jlambre1/cjtzjn4x
 	maxZoom: 19,
 	attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+//zoom control options
+var zoomOptions = {
+	zoomInText: '+',
+	zoomOutText: '-',
+	position: 'topright',
+};
+var zoom = L.control.zoom(zoomOptions); //Creating Zoom Control
+zoom.addTo(map); //Adding Zoom Control to map
