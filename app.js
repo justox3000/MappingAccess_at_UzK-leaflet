8389e834b4f8553f5ff9fc30513539981863704d
Justus Lambrecht <justuslambrecht@icloud.com>
2019-08-20 11:19:51 +0200

    Switch Basemaps

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Initialize leaflet.js
 var L = require('leaflet');
 
-
 /*
 When pulling in GeoJSON from external data source or hosting it locally,
 you’ll need to load the data using AJAX. jQuery provides a standard getJSON function
@@ -10,21 +9,52 @@ leaflet plugin: leaflet ajax
  Add GeoJSON data via ajax or jsonp.
 */
 
-// Initialize the map and set
-var map = L.map('map', {
-  //zoom control options
-  scrollWheelZoom: true,
-  zoomControl: false,
+//OSM tiles attribution and URL
+var osmLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+var osmURL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib = '&copy; ' + osmLink;
+
+//Carto tiles attribution and URL
+var cartoLink = '<a href="http://cartodb.com/attributions">CartoDB</a>';
+var cartoURL = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
+var cartoAttrib = '&copy; ' + osmLink + ' &copy; ' + cartoLink;
+
+//Stamen Toner tiles attribution and URL
+var stamenURL = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}';
+var stamenAttrib = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+
+//Creation of map tiles
+var osmMap = L.tileLayer(osmURL, {attribution: osmAttrib});
+var cartoMap = L.tileLayer(cartoURL, {attribution: cartoAttrib});
+var stamenMap = L.tileLayer(stamenURL,{
+  attribution: stamenAttrib,
+  subdomains: 'abcd',
+ 
+  ext: 'png'
 });
+
+//Map creation
+var map = L.map('map',{
+  layers: [osmMap]
+}).setView([50.927, 6.931], 16);
+
+//Base layers definition and addition
+var baseLayers = {
+  "OSM Mapnik": osmMap,
+  "Carto DarkMatter": cartoMap,
+  "Stamen Toner": stamenMap
+};
+
+ //Add baseLayers to map as control layers
+ L.control.layers(baseLayers).addTo(map);
+
+
+
 
 //create custom zoom Control on right bottom
 var zoomControl = L.control.zoom(
   zoomOptions = {position: 'bottomright'}
 ).addTo(map);
-
-// Set the position and zoom level of the map
-//later set so that we always automatically zoom to see the data
-map.setView([50.927, 6.931], 16);
 
 // Initialize the base layer
 //offer alternative base layer; include base layer choice toggle on top right of mapbox
@@ -32,5 +62,6 @@ var osm_mapnik = L.tileLayer('https://api.mapbox.com/styles/v1/jlambre1/cjtzjn4x
 	maxZoom: 19,
 	attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
 
 var toilets_Layer = new L.GeoJSON.AJAX("https://raw.githubusercontent.com/justox3000/MappingAccess-UzK-leaflet/master/data/plain_geojsons/toilets.geojson");
