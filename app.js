a9b09810822f3f789b4491d554f43df88c3a1a58
Justus Lambrecht <justuslambrecht@icloud.com>
2019-08-26 19:34:08 +0200

    Edit of Basemaps

--- app.js
+++ app.js
@@ -25,37 +25,6 @@ var osm_de = L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{
 	attribution: '&copy; OSM Deutschland <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
-var osm_fr = L.tileLayer('http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-	maxZoom: 20,
-	attribution: '&copy; OSM France | &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
-
-var osm_hot = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; OSM Hot <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
-});
-
-var osm_topo = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-});
-
-var stamen_Toner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
-});
-
-var stamen_TonerBackground = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
-});
-
 var stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	subdomains: 'abcd',
@@ -64,13 +33,6 @@ var stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
 	ext: 'png'
 });
 
-var stamen_Watercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'png'
-});
 
 var stamen_Terrain = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -80,64 +42,45 @@ var stamen_Terrain = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terra
 	ext: 'png'
 });
 
-var stamen_TerrainBackground = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 18,
-	ext: 'png'
-});
-
-var esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-});
-
-var esri_WorldTerrain = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
-	maxZoom: 13
-});
-
-var esri_NatGeoWorldMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
-	maxZoom: 16
-});
 
 // Create base layers group object
 var baseLayers = {
 	"OSM Mapnik": osm_mapnik,
 	"OSM Black White Mapnik": osm_bw_mapnik,
 	"OSM Germany": osm_de,
-	"OSM France": osm_fr,
-	"OSM Hot": osm_hot,
-	"OSm Topo": osm_topo,
-	"Stamen Toner": stamen_Toner,
-	"Stamen Toner Background": stamen_TonerBackground,
 	"Stamen Toner Lite": stamen_TonerLite,
-	"Stamen Watercolor": stamen_Watercolor,
-	"Stamen Terrain": stamen_Terrain,
-	"Stamen Terrain Background": stamen_TerrainBackground,
-	"ESRI World Imagery": esri_WorldImagery,
-	"ESRI World Terrain": esri_WorldTerrain,
-	"ESRI National Geographic": esri_NatGeoWorldMap
+	"Stamen Terrain": stamen_Terrain
 };
 
 // Add baseLayers to the map
 L.control.layers(baseLayers, null).addTo(map);
 
+var controlLayers = L.control.layers().addTo(map);
+
 //load GeoJSON buildings
-$.getJSON("../data/a11y_uzk/a11y_buildings.geojson", function(data){
+var buildings = $.getJSON("../data/a11y_uzk/a11y_buildings.geojson", function(data){
 	//add GeoJSON layer to the map once the file is loaded
 	L.geoJSON(data).addTo(map);
+
+	controlLayers.addOverlay(geojsonLayer, 'Title');
 });
 
 //load GeoJSON family campus
-$.getJSON("../data/a11y_uzk/a11y_family_campus.geojson", function(data){
+var family = $.getJSON("../data/a11y_uzk/a11y_family_campus.geojson", function(data){
 	//add GeoJSON layer to the map once the file is loaded
 	L.geoJSON(data).addTo(map);
 });
 
 //load GeoJSON parking
-$.getJSON("../data/a11y_parking.geojson", function(data){
+var parking = $.getJSON("../data/a11y_parking.geojson", function(data){
 	//add GeoJSON layer to the map once the file is loaded
 	L.getJSON(data).addTo(map);
 });
+
+var overlayMaps = {
+	"Buildings": buildings,
+	"Family Campus": family,
+	"Parking": parking
+};
+
+L.control.layers(null, overlayMaps).addTo(map);
